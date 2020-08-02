38cb3d7084a086bbf2544e90b93e3e9b260bbe64
Juzer Ali <juzerali@live.com>
2020-08-02 11:00:30 +0530

    refactor: use async/await all around

--- index.js
+++ index.js
@@ -165,59 +165,64 @@ async function showCreateWidget() {
     $("#create-new-game-widget").removeClass("hidden");
     $("#game-window").addClass("hidden");
 
-    let previousList = $("#previous-games-list").empty();
+    let $existingGamesList = $("#previous-games-list").empty();
     let result = await gameDB.allDocs();
-    result.rows.forEach(function (row, i) {
+    let games = await Promise.all(result.rows.map(async (row) => {
+            return gameDB.get(row.id);
+        })
+    );
+
+    games = games.map((g => new HousieGame(g._id, g.name, g.drawn, new Date(g.createdAt))))
+        .sort((a, b) => a.createdAt.getTime() > b.createdAt.getTime() ? -1 : 1);
+
+    games.forEach(await async function (game, i) {
         let shortKey = i + 1;
-        let id = row.id;
-        gameDB.get(row.id).then(function (game) {
-            game = new HousieGame(row.id, game.name, game.drawn, new Date(game.createdAt));
-            let li = $("<li>");
+        let id = game._id;
+        let li = $("<li>");
+
+        game = new HousieGame(id, game.name, game.drawn, new Date(game.createdAt));
+
+        function wrapperFor(game) {
             let a = $("<a>");
-            let span = $("<span>");
-            let icon = $(`<span><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-keyboard-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M0 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6zm13 .25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM2.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 3 8.75v-.5A.25.25 0 0 0 2.75 8h-.5zM4 8.25A.25.25 0 0 1 4.25 8h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 4 8.75v-.5zM6.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 7 8.75v-.5A.25.25 0 0 0 6.75 8h-.5zM8 8.25A.25.25 0 0 1 8.25 8h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 8 8.75v-.5zM13.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm0 2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm-3-2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-1.5zm.75 2.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM11.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zM9 6.25A.25.25 0 0 1 9.25 6h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 9 6.75v-.5zM7.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 8 6.75v-.5A.25.25 0 0 0 7.75 6h-.5zM5 6.25A.25.25 0 0 1 5.25 6h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 5 6.75v-.5zM2.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5A.25.25 0 0 0 4 6.75v-.5A.25.25 0 0 0 3.75 6h-1.5zM2 10.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM4.25 10a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h5.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-5.5z"/></svg> ${shortKey} </span>`)
-            a.attr("href", "#" + row.id).text(game.name);
-            span.text(` (${game.createdAtReadable()})`);
-
-            let wrapper = $("<span></span>")
-                .append(icon)
-                .append(a)
-                .append(span);
-            li.append(wrapper)
-            previousList.append(li);
-
+            let createdAtField = $("<span>");
+            let keyboardIcon = $(`<span><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-keyboard-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M0 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6zm13 .25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM2.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 3 8.75v-.5A.25.25 0 0 0 2.75 8h-.5zM4 8.25A.25.25 0 0 1 4.25 8h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 4 8.75v-.5zM6.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 7 8.75v-.5A.25.25 0 0 0 6.75 8h-.5zM8 8.25A.25.25 0 0 1 8.25 8h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 8 8.75v-.5zM13.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm0 2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm-3-2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-1.5zm.75 2.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM11.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zM9 6.25A.25.25 0 0 1 9.25 6h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 9 6.75v-.5zM7.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 8 6.75v-.5A.25.25 0 0 0 7.75 6h-.5zM5 6.25A.25.25 0 0 1 5.25 6h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 5 6.75v-.5zM2.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5A.25.25 0 0 0 4 6.75v-.5A.25.25 0 0 0 3.75 6h-1.5zM2 10.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM4.25 10a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h5.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-5.5z"/></svg> ${shortKey} </span>`)
+            a.attr("href", "#" + id).text(game.name);
             a.on("click", function () {
-                window.load(row.id);
+                window.load(game._id);
             });
-            $(document).on("keyup", (e) => {
-                if (e.key == shortKey) {
-                    window.load(id);
-                }
+            createdAtField.text(` (${game.createdAtReadable()})`);
+            let deleteIcon = $(`<a href="#"><svg color="red" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/></svg></a>`)
+            deleteIcon.click(async () => {
+                let doc = await gameDB.get(id);
+                gameDB.remove(doc);
+                let undo = $(`<a href='#'>Restore ${doc.name} ↩</a>`);
+                li.empty().append(undo);
+
+                undo.click(async () => {
+                    let newdoc = await gameDB.put({
+                        _id: window.randomId(),
+                        name: doc.name,
+                        drawn: doc.drawn,
+                        createdAt: doc.createdAt
+                    });
+                    window.showCreateWidget();
+                });
             });
 
-            let deleteIcon = $(`<a href="#"><svg color="red" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/></svg></a>`)
-            deleteIcon.click(() => {
-                gameDB.get(id).then(doc => {
-                    gameDB.remove(doc);
-                    let undo = $("<a href='#'>Undo ↩</a>");
-                    li.empty().append(undo);
-                    undo.click(() => {
-                        gameDB.put({
-                            _id: window.randomId(),
-                            name: doc.name,
-                            drawn: doc.drawn,
-                            createdAt: doc.createdAt
-                        }).then((doc) => {
-                            a
-                                .attr("href", "#" + doc.id)
-                                .off("click")
-                                .on("click", () => window.load(doc.id))
-                            li.empty().append(wrapper)
-                        });
-                    });
-                })
-            });
-            li.append(deleteIcon);
+            return $("<span></span>")
+                .append(keyboardIcon)
+                .append(a)
+                .append(createdAtField)
+                .append(deleteIcon);
+        }
+
+        let wrapper = wrapperFor(game);
+        li.append(wrapper).appendTo($existingGamesList);
+
+        $(document).on("keyup", (e) => {
+            if (e.key == shortKey) {
+                window.load(id);
+            }
         });
     });
 }
@@ -229,18 +234,18 @@ function showGameWindow() {
     $(document)
         .off("keyup")
         .on("keyup", (e) => {
-        if (e.key === "D" || e.key === "d" || e.key === " ") {
-            window.draw();
-        } else if (e.key === "Backspace" || e.key === "n" || e.key === "u") {
-            window.showCreateWidget();
-        } else if (e.key === "m") {
-            window.APP.toggleMute();
-        }
-    });
+            if (e.key === "D" || e.key === "d" || e.key === " ") {
+                window.draw();
+            } else if (e.key === "Backspace" || e.key === "n" || e.key === "u") {
+                window.showCreateWidget();
+            } else if (e.key === "m") {
+                window.APP.toggleMute();
+            }
+        });
 }
 
 function randomId() {
-    return (Math.random() * Math.pow(10, 16)).toString(36);
+    return (new Date).getTime().toString(36);
 }
 
 window.speaker = function (number) {
