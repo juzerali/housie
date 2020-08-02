bd9541bb5f2b116e3d0919bf56bab1049dc19561
Juzer Ali <juzerali@live.com>
2020-08-02 11:54:10 +0530

    no blink on restore

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ class HousieGame {
     }
 }
 
+HousieGame.fromDoc = (doc) => new HousieGame(doc._id, doc.name, doc.drawn, new Date(doc.createdAt));
+
 $(async () => {
     let gameDB = new PouchDB("games");
     let prefsDB = new PouchDB("prefs");
@@ -178,11 +180,11 @@ async function showCreateWidget() {
     games.forEach(await async function (game, i) {
         let shortKey = i + 1;
         let id = game._id;
-        let li = $("<li>");
+        let $li = $("<li>");
 
         game = new HousieGame(id, game.name, game.drawn, new Date(game.createdAt));
 
-        function wrapperFor(game) {
+        function insertLineItem(game, li) {
             let a = $("<a>");
             let createdAtField = $("<span>");
             let keyboardIcon = $(`<span><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-keyboard-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M0 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6zm13 .25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM2.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 3 8.75v-.5A.25.25 0 0 0 2.75 8h-.5zM4 8.25A.25.25 0 0 1 4.25 8h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 4 8.75v-.5zM6.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 7 8.75v-.5A.25.25 0 0 0 6.75 8h-.5zM8 8.25A.25.25 0 0 1 8.25 8h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 8 8.75v-.5zM13.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm0 2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm-3-2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-1.5zm.75 2.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM11.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zM9 6.25A.25.25 0 0 1 9.25 6h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 9 6.75v-.5zM7.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 8 6.75v-.5A.25.25 0 0 0 7.75 6h-.5zM5 6.25A.25.25 0 0 1 5.25 6h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 5 6.75v-.5zM2.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5A.25.25 0 0 0 4 6.75v-.5A.25.25 0 0 0 3.75 6h-1.5zM2 10.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM4.25 10a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h5.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-5.5z"/></svg> ${shortKey} </span>`)
@@ -194,7 +196,7 @@ async function showCreateWidget() {
             let deleteIcon = $(`<a href="#"><svg color="red" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/></svg></a>`)
             deleteIcon.click(async () => {
                 let doc = await gameDB.get(id);
-                gameDB.remove(doc);
+                await gameDB.remove(doc);
                 let undo = $(`<a href='#'>Restore ${doc.name} ↩</a>`);
                 li.empty().append(undo);
 
@@ -205,7 +207,10 @@ async function showCreateWidget() {
                         drawn: doc.drawn,
                         createdAt: doc.createdAt
                     });
-                    window.showCreateWidget();
+                    newdoc = await gameDB.get(newdoc.id);
+                    id = newdoc._id;
+                    game = HousieGame.fromDoc(newdoc);
+                    insertLineItem(game, li)
                 });
             });
 
@@ -213,11 +218,12 @@ async function showCreateWidget() {
                 .append(keyboardIcon)
                 .append(a)
                 .append(createdAtField)
-                .append(deleteIcon);
+                .append(deleteIcon)
+                .appendTo(li.empty());
         }
 
-        let wrapper = wrapperFor(game);
-        li.append(wrapper).appendTo($existingGamesList);
+        insertLineItem(game, $li);
+        $li.appendTo($existingGamesList);
 
         $(document).on("keyup", (e) => {
             if (e.key == shortKey) {
